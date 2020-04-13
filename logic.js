0e311bb17ecf6318bfe8633f406a33d8a022c95c
Leonardo M. De Ugarte R <leonardodeu@outlook.com>
2020-04-12 18:13:40 -0700

    Feature = Add todo complete

--- logic.js
+++ logic.js
@@ -3,8 +3,14 @@ let prompt = document.getElementById('js-prompt');
 let inputName = document.getElementById('js-input-name');
 let button = document.getElementById('js-button');
 
-let todo = document.getElementById('js-todo');
 let displayName = document.getElementById('js-display-name');
+let todo = document.getElementById('js-todo');
+let inputTodo = document.getElementById('js-input-todo');
+let buttonNew = document.getElementById('js-button-new');
+
+let list = document.getElementById('js-list');
+let listItem = '<div class="list-item"><span>trash</span><p class="insert"></p><button>check</button></div>';
+
 
 //State
 
@@ -16,3 +22,21 @@ function getName(){
   displayName.innerText = inputName.value;
 };
 
+// function reRun(){
+//   for(let x = 0; x < btnNext.length; x++){
+//     btnNext[x].addEventListener('click', addTodo);
+//   };
+//   console.log(btnNext.length);
+// };
+// reRun();
+buttonNew.addEventListener('click', addTodo);
+function addTodo(){
+  //check any text exist in the previous one before adding
+  //add classlist styling
+  //Add Item to List
+  list.insertAdjacentHTML('beforeend', listItem);
+  let demo = document.querySelectorAll('.insert');
+  demo[demo.length - 1].innerText = inputTodo.value;
+  //Make Blank
+  inputTodo.value = "";
+};
